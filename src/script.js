36249e3808fa30c402e07c27724713c08dfcbaee
Teresa Morini <tjmcodes@gmail.com>
2022-11-03 15:39:36 +0000

    tidy up

--- src/script.js
+++ src/script.js
@@ -1,33 +1,6 @@
-// keys values / array of objects
-// const colours = {
-//   "aqua"  : 0, 
-//   "green" : 1, 
-//   "blue"  : 2, 
-//   "red"   : 3, 
-//   "pink"  : 4, 
-//   "grey"  : 5, 
-//   "black" : 6, 
-//   "purple": 7, 
-//   "yellow": 8, 
-//   "white" : 9, 
-// }
+// ! Results container
 
-// const items = Object.keys(colours).map(function(key) {
-//   return [key, colours[key]]
-// })
 const emojis = ['❤️', '🌟', '🐍', '📍']
-
-
-
-// items.sort(function(first, second) {
-//   return second[1] - first[1];
-// })
-
-// console.log(items.slice(0,10))
-
-
-// const coloursArray = ["1", "2", "3"]
-
 
 function random() {
   const number = Math.floor(Math.random() * emojis.length)
@@ -42,38 +15,7 @@ function randomNumberSelector() {
   const num2 = document.querySelector('#num2').innerHTML = random()
   const num3 = document.querySelector('#num3').innerHTML = random()
   
-  // ! Results as a string
-  const resultString = `${num1}${num2}${num3}`
-  resultString.split()
-  
-  results.push(resultString)  
-  
-  // ! Result as an array
-  // const resultsInt = parseInt(resultString)
-  
-  console.log(results)
-  console.log(resultString)
-  console.log(typeof(resultString))
-  // console.log(parseInt(resultString))
-  
-  
-  // ! Print out individual elements in array
-  let i = 0;
-  
-  while (i < resultString.length) {
-    console.log(resultString[i]);
-    i++;
-  } 
 
-  for (const element of resultString) {
-    console.log(element)
-  }
-
-  
-  
-  console.log(results)
 } 
-
-// console.log(coloursArray) 
 
 
