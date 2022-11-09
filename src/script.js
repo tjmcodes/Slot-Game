b3181343d27fe12b9211292006ffe4276811a9a8
luke-o-brien <lukeobrien02@gmail.com>
2022-11-09 19:51:30 +0000

    Points Updated added to win if statement function

--- src/script.js
+++ src/script.js
@@ -5,34 +5,33 @@ let wintype = null
 let points = 100
 
 function token() {
-  points = points - 25
-  window.alert("-25 points")
-}
-
-function scoreboard () {
   const scoreboard = document.querySelector('#score')
-  switch (wintype) {
-      case 0:
-        points = points
-        scoreboard.innerHTML = points
-        break;
-      case 2:
-        wintype = "+25 points"
-        points = points +25
-        scoreboard.innerHTML = points
-          break;
-      case 3:
-        wintype = "+50 points" 
-        points = points + 50
-        scoreboard.innerHTML = points
-        break;
-      default:
-        break;
-    }
-  console.log(points)
-  console.log(wintype)
-
+  points = points - 25
+  scoreboard.innerHTML = points
+  // window.alert("-25 points")
 }
+
+// function scoreboard () {
+//   const scoreboard = document.querySelector('#score')
+//   switch (wintype) {
+//       case 0:
+//         points = points
+//         break;
+//       case 2:
+//         wintype = "+25 points"
+//           break;
+//       case 3:
+//         wintype = "+50 points" 
+//         points = points + 100
+//         scoreboard.innerHTML = points
+//         break;
+//       default:
+//         break;
+//     }
+//   console.log(points)
+//   console.log(wintype)
+
+// }
 
 function random() {
   const number = Math.floor(Math.random() * emojis.length)
@@ -59,20 +58,28 @@ function randomNumberSelector() {
 
 
 function win () {
+  const scoreboard = document.querySelector('#score')
   if ((num1.innerHTML === num2.innerHTML) && (num1.innerHTML === num3.innerHTML)) {
+    points = points + 100
+    scoreboard.innerHTML = points
     console.log("win +50 points")
     return wintype = 3
     // window.alert("you win +50 points")
   } else if (num1.innerHTML === num2.innerHTML || num1.innerHTML === num3.innerHTML) {
     console.log("win +25 points")
+    points = points +50
+    scoreboard.innerHTML = points
     return wintype = 2
     // return window.alert("2 Matches! +25 points")
   } else if (num2.innerHTML === num3.innerHTML) {
     console.log("win +25 points statement 3")
+    points = points +50
+    scoreboard.innerHTML = points
     return wintype = 2
     // return window.alert("2 Matches! +25 points")
   } else {
     console.log("sorry you don't get anypoints")
+    scoreboard.innerHTML = points
     return wintype = 0
     // return window.alert("No Matches sorry 0 points")
   }
